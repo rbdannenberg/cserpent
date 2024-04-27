f00f2c3172dbff7ded094d91a7651930aa4a7e5c
Linxuan M <davidmaamoaix@gmail.com>
2024-04-27 17:59:44 -0400

    [CI] Python unit test handler init

--- toolchain/srp_tests.py
+++ toolchain/srp_tests.py
@@ -1,2 +1,16 @@
-# TODO: add tests
-print("Hello World")
+import os
+import unittest
+import subprocess
+
+
+SRP_TESTS = 'tests/serpent'
+
+
+class TestExitCode(unittest.TestCase):
+    pass
+
+
+for root, _, files in os.walk(SRP_TESTS):
+    for file in files:
+        file_name = os.path.join(root, file)
+        subprocess.run(["serpent64", file_name])
