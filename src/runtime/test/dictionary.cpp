c6d2d7cabb1cfa78894edd4f18433cf118399d48
Anthony Yip <anthony.ch.yip@gmail.com>
2024-06-06 14:32:22 +0800

    Implementation and testing of dictionaries (simple solution - multiple inheritance)

--- src/runtime/test/dictionary.cpp
+++ src/runtime/test/dictionary.cpp
@@ -1,11 +1,12 @@
 //
 // Created by anthony on 6/4/24.
 //
-#include <any.h>
-#include <data_structures/dictionary.h>
+#include "any.h"
+#include "data_structures/dictionary.h"
+#include "op_overload.h"
 #include <iostream>
 
-int main() {
+void test_dictionary() {
     Dictionary& D_native = *(new Dictionary {});
     Any D = *(new Dictionary {});
     D['a'] = 5;
@@ -15,5 +16,4 @@ int main() {
     std::cout << to_real(D['a']) << std::endl;
 
     //D[6] = "hello";
-
 }
